7302b27f0610cd8a988d415866a7ba8b03b0f50a
Chester Frasier <c.frasier163@gmail.com>
2022-09-29 16:23:03 -0700

    first commit

---
+++ cfmethod1.py
@@ -0,0 +1,16 @@
+
+def sqft():
+    Length = float(input('What is the length? '))
+    Width = float(input('What is the width? '))
+    area = Length * Width 
+    print(area) 
+sqft()
+
+
+from math import pi
+def circumference():
+    radius = float(input('What is the radius? '))
+    circumference = 2 * radius * pi 
+    print(circumference)
+
+circumference()
